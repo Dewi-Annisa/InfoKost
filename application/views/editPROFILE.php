05166364844390d8acb748b66a340b3ed8c2e0d1
Dewi-Annisa <47683285+Dewi-Annisa@users.noreply.github.com>
2019-04-23 23:50:00 +0700

    Update editPROFILE.php

--- application/views/editPROFILE.php
+++ application/views/editPROFILE.php
@@ -1,22 +1,22 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <title>Edit Akun - InfoKos</title>
+  <title>Edit Akun - InfoKost</title>
   
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/PROFILEedit.css')?>">
 </head>
 <body>
-
-<div class="container">
-  <form>
-    <div class="row">
-      <div class="col-25">
-        <label for="name">Nama Lengkap</label>
-      </div>
+  <body style="background-image: url(assets/image/kost.jpg)"></body>
+  <div class="container">
+    <form action="/action_page.php">
+      <div class="row">
+        <div class="col-25">
+          <label for="name">Nama Lengkap</label>
+        </div>
       <div class="col-75">
-        <input type="text" id="fname" name="name" placeholder="Nama Pertama">
-        <input type="text" id="fname" name="name" placeholder="Nama Terakhir">
+        <input type="text" id="fname" name="name" placeholder="First Name">
+        <input type="text" id="fname" name="name" placeholder="Last Name">
       </div>
     </div>
     <div class="row">
@@ -25,14 +25,14 @@
       </div>
       <div class="col-75">
         <select id="city" name="city">
-          <option value="jkt">Laki-Laki</option>
-          <option value="sby">Perempuan</option>
+          <option value="lk">Laki-Laki</option>
+          <option value="pr">Perempuan</option>
         </select>
       </div>
     </div>
     <div class="row">
       <div class="col-25">
-        <label for="status">No. Handphone</label>
+        <label for="tlp">No. Handphone</label>
       </div>
       <div class="col-75">
         <input type="text" id="fname" name="name" placeholder="xxxx xxxx">
@@ -48,17 +48,17 @@
     </div>
     <div class="row">
       <div class="col-25">
-        <label for="status">Password</label>
+        <label for="pass">Password</label>
       </div>
       <div class="col-75">
-        <input type="text" id="fname" name="name" placeholder="Password">
+        <input type="password" class="form-control" id="pwd" placeholder="Enter password" name="pswd">
       </div>
     </div>
     <div class="row">
       <a href="application/views/profil.php">
         <input type="submit" value="Simpan">
       </a>
-      <a href="profil.html">
+      <a href="application/views/profil.php">
         <input type="button" value="Buang">
       </a>
     </div>
